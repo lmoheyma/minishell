c6956843a6c74e5f34da203f0a4e5261bc4c6625
antoine <antoine@aleite-b.aleite.virtualbox.org>
2024-01-18 19:30:00 +0100

    args without quotes

---
+++ libft/ft_split_args.c
@@ -0,0 +1,138 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_args.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 11:51:40 by aleite-b          #+#    #+#             */
+/*   Updated: 2024/01/18 19:26:56 by antoine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_trigger(char c, char *trigger)
+{
+	int	i;
+
+	i = 0;
+	if (c == '\'' && *trigger == '/')
+	{
+		*trigger = '\'';
+		i++;
+	}
+	else if (c == '\"' && *trigger == '/')
+	{
+		*trigger = '\"';
+		i++;
+	}
+	else if (c == *trigger && *trigger != '/')
+	{
+		*trigger = '/';
+		i++;
+	}
+	return (i);
+}
+
+static int	count_str(char const *s)
+{
+	int		i;
+	int		words;
+	char	trigger;
+
+	words = 0;
+	i = 0;
+	trigger = '/';
+	while (s[i])
+	{
+		if (s[i] == '\'' && trigger == '/')
+			trigger = '\'';
+		else if (s[i] == '\"' && trigger == '/')
+			trigger = '\"';
+		else if (s[i] == trigger && trigger != '/')
+			trigger = '/';
+		if ((s[i + 1] == ' ' || s[i + 1] == '\0') && (s[i] != ' '
+				|| s[i] != '\0') && trigger == '/')
+			words++;
+		i++;
+	}
+	return (words);
+}
+
+static int	get_len(char *s, int add)
+{
+	int		i;
+	int		len;
+	char	trigger;
+
+	i = 0;
+	len = 0;
+	trigger = '/';
+	while ((s[i + len] != ' ' && s[i + len]) || (trigger != '/' && s[i + len]))
+	{
+		if (ft_trigger(s[i + len], &trigger))
+			i++;
+		len++;
+	}
+	if (add)
+		return (len + i);
+	else
+		return (len);
+}
+
+static char	*write_word(char *s)
+{
+	int		i;
+	int		j;
+	char	*str;
+	char	trigger;
+
+	i = 0;
+	j = 0;
+	trigger = '/';
+	str = malloc(sizeof(char) * (get_len(s + i, 0) + 1));
+	if (!str)
+		return (NULL);
+	while ((s[i + j] != ' ' && s[i + j]) || (trigger != '/' && s[i + j]))
+	{
+		if (ft_trigger(s[i + j], &trigger))
+			j++;
+		str[i] = s[i + j];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	**ft_split_args(char *s)
+{
+	char	**str;
+	int		words;
+	int		i;
+	int		j;
+
+	words = count_str(s);
+	str = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
+			i++;
+		str[j] = write_word(s + i);
+		if (!str[j])
+		{
+			j--;
+			while (j--)
+				free(str[j]);
+			return (NULL);
+		}
+		j++;
+		i += get_len(s + i, 1);
+	}
+	str[j] = 0;
+	return (str);
+}
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 11:56:47 by aleite-b          #+#    #+#             */
-/*   Updated: 2024/01/18 17:52:44 by antoine          ###   ########.fr       */
+/*   Updated: 2024/01/18 19:27:32 by antoine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,16 @@ void	parse_all_minishell(t_minishell *minishell, char *cmd)
 	// 		minishell->tokens->type);
 	// 	minishell->tokens = minishell->tokens->next;
 	// }
+	// int i = 0;
 	// while (minishell->args)
 	// {
-	// 	printf("Args : %s\n", minishell->args->cmd[0]);
+	// 	while (minishell->args->cmd[i])
+	// 	{
+	// 		printf("Args");
+	// 		printf(" - %s", minishell->args->cmd[i]);
+	// 		printf("\n");
+	// 		i++;
+	// 	}
 	// 	minishell->args = minishell->args->next;
 	// }
 	// ft_err(minishell, "", 2);
