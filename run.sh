332dc2d96d0ac171c48bccd76d4dc4f78c06bfee
Louis Moheymani <lmoheyma@bess-f2r3s16.clusters.42paris.fr>
2024-01-30 21:15:11 +0100

    fix : valgrind script

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 
-if [ $1 == "log" ]
-then
+if [[ $1 == "log" ]]; then
 	valgrind --leak-check=full --show-reachable=yes --error-limit=no --suppressions=./valgrind-readline.supp --gen-suppressions=all --log-file=minishell.log --track-fds=yes ./minishell
 else
 	valgrind --leak-check=full --show-reachable=yes --error-limit=no --suppressions=./valgrind-readline.supp --gen-suppressions=all --track-fds=yes ./minishell
